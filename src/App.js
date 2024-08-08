51a4abb44ae73a4cc2c55b9828041c0664e1e4f4
yuriy05 <dilayyura@gmail.com>
2024-08-08 23:59:42 +0300

    feat: added close button & improve next/previous buttons logic

--- src/App.js
+++ src/App.js
@@ -8,33 +8,35 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   const handleNextStep = () => {
-    if (step === 3) {
-      setStep(3);
-    } else setStep(prevStep => prevStep + 1);
+    if (step < 3) setStep(prevstate => prevstate + 1)
   }
 
   const handlePreviousStep = () => {
-    if (step === 1) {
-      setStep(1);
-    } else setStep(prevStep => prevStep - 1);
+    if (step > 1) setStep(prevstate => prevstate - 1)
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? 'active' : ""}`}>1</div>
-        <div className={`${step >= 2 ? 'active' : ""}`}>2</div>
-        <div className={`${step >= 3 ? 'active' : ""}`}>3</div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>&times;</button>
+      {isOpen && (
+      <div className="steps">
+        <div className="numbers">
+          <div className={`${step >= 1 ? 'active' : ""}`}>1</div>
+          <div className={`${step >= 2 ? 'active' : ""}`}>2</div>
+          <div className={`${step >= 3 ? 'active' : ""}`}>3</div>
+        </div>
+  
+        <p className="message">Step {step}: {messages[step - 1]}</p>
+  
+        <div className="buttons">
+          <button onClick={handlePreviousStep} style={{backgroundColor: '#7950f2', color: '#fff'}}>Previous</button>
+          <button onClick={handleNextStep} style={{backgroundColor: '#7950f2', color: '#fff'}}>Next</button>
+        </div>
       </div>
-
-      <p className="message">Step {step}: {messages[step - 1]}</p>
-
-      <div className="buttons">
-        <button onClick={handlePreviousStep} style={{backgroundColor: '#7950f2', color: '#fff'}}>Previous</button>
-        <button onClick={handleNextStep} style={{backgroundColor: '#7950f2', color: '#fff'}}>Next</button>
-      </div>
-    </div>
+      )}
+    </>
   )
 };
